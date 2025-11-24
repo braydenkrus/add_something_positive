a53c967f1e39de5f5064af60dfddfbed9fed03ca
Brayden Krus <151576863+braydenkrus@users.noreply.github.com>
2025-11-24 17:20:02 -0500

    Fix spinner

--- frontend/src/renderer/src/App.jsx
+++ frontend/src/renderer/src/App.jsx
@@ -4,15 +4,21 @@ import Entry from './components/Entry'
 import { useState } from 'react'
 
 function App() {
+  // current entry in textbox
   const [entryQuery, setEntryQuery] = useState('')
 
+  // set of entries
   const [entries, setEntries] = useState([])
 
+  // choose what to render based on if we are ready
   const [readyToDisplayEntries, setReady] = useState(false)
 
+  // for showing the spinner
+  const [showSpinner, setShowSpinner] = useState(false)
   // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
   // save entry, retrieve entries
   const saveEntry = async (e) => {
+    setShowSpinner(true)
     e.preventDefault()
     const response = await fetch('http://127.0.0.1:5000/flask/write', {
       method: 'POST',
@@ -24,7 +30,6 @@ function App() {
         entryQuery +
         "\nLet's take a look at some other positive things you've entered!"
     )
-    console.log(response)
     const fetchedEntries = await fetch('http://127.0.0.1:5000/flask/entries', {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' }
@@ -33,15 +38,19 @@ function App() {
     const finalEntries = await fetchedEntries.json()
     setEntries(finalEntries)
     setReady(true)
+    setEntryQuery('') // TODO: part of fixing Issue #4, not complete yet.
+    setShowSpinner(false)
   }
   // delete entry, update database, retrieve
   const executeDelete = async (id) => {
+    setShowSpinner(true)
     setReady(false)
     const deletion = await fetch('http://127.0.0.1:5000/flask/delete', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ delete_entry: id })
     })
+    // TODO: Randomizing the order does not make sense for deletion.
     const fetchedEntries = await fetch('http://127.0.0.1:5000/flask/entries', {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' }
@@ -50,6 +59,7 @@ function App() {
     const finalEntries = await fetchedEntries.json()
     setEntries(finalEntries)
     setReady(true)
+    setShowSpinner(false)
   }
 
   return (
@@ -86,10 +96,12 @@ function App() {
               />
             ))}
           </div>
-        ) : (
+        ) : showSpinner ? (
           <div className="spinner-border text-primary" role="status">
             <span className="visually-hidden">Loading...</span>
           </div>
+        ) : (
+          <div></div>
         )}
       </div>
     </>
