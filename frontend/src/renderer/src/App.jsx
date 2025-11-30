ecfe33b21c5ddea235761c9e7fcee0a3ef0de191
Brayden Krus <151576863+braydenkrus@users.noreply.github.com>
2025-11-30 16:47:51 -0500

    Fix Issue #4
    Uses Modal from react-bootstrap.

--- frontend/src/renderer/src/App.jsx
+++ frontend/src/renderer/src/App.jsx
@@ -1,6 +1,8 @@
 import Entry from './components/Entry'
-import DeleteAlert from './components/DeleteAlert.jsx'
 import { useState } from 'react'
+// React Bootstrap for modals
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
 function App() {
   // current entry in textbox
@@ -15,11 +17,30 @@ function App() {
   // for showing the spinner
   const [showSpinner, setShowSpinner] = useState(false)
 
-  // for showing delete alert message
-  // const [showDeleteAlert, setShowDeleteAlert] = useState(false)
+  // for entry submission modal
+  const [showSubmissionModal, setShowSubmissionModal] = useState(false)
 
-  // need to fix
-  // const [readyToDelete, setReadyToDelete] = useState(true)
+  const handleSubmissionClose = () => {
+    setShowSubmissionModal(false)
+    setEntryQuery('')
+  }
+
+  const handleSubmissionOpen = () => setShowSubmissionModal(true)
+
+  // for deletion modal
+  const [showDeletionModal, setShowDeletionModal] = useState(false)
+
+  const handleDeletionClose = () => setShowDeletionModal(false)
+
+  const handleDeletionOpen = () => setShowDeletionModal(true)
+
+  // to take note of what is currently being deleted
+  const [entryToDelete, setEntryToDelete] = useState(-1)
+
+  const handleWantToDeleteClose = () => {
+    setReadyToDelete(true)
+    handleDeletionClose()
+  }
 
   // https://www.dhiwise.com/post/a-step-by-step-guide-to-retrieving-input-values-in-react
   // to be more explicit with how I'm writing this. Also, this should be a good resource
@@ -32,22 +53,15 @@ function App() {
   // save entry, retrieve entries
   const saveEntry = async (e) => {
     e.preventDefault()
-    // https://www.w3schools.com/Jsref/met_form_reset.asp
-    document.getElementById('queryForm').reset()
     // https://github.com/electron/electron/issues/22923 May help with Issue #4.
-    // Based on this, I should follow through with making custom popups.
-    setEntryQuery('') // TODO: part of fixing Issue #4, not complete yet.
+    // This was the fix!
     setShowSpinner(true)
+    handleSubmissionOpen()
     const response = await fetch('http://127.0.0.1:5000/flask/write', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ new_entry: entryQuery })
     })
-    alert(
-      'You entered: ' +
-        entryQuery +
-        "\nLet's take a look at some other positive things you've entered!"
-    )
     const fetchedEntries = await fetch('http://127.0.0.1:5000/flask/entries', {
       method: 'GET',
       headers: { 'Content-Type': 'application/json' }
@@ -57,36 +71,34 @@ function App() {
     setEntries(finalEntries)
     setReady(true)
     setShowSpinner(false)
+    // https://www.w3schools.com/Jsref/met_form_reset.asp
+    document.getElementById('queryForm').reset()
   }
   // delete entry, update database, retrieve
-  const executeDelete = async (id) => {
-    /* 
-    Note that this is an Electron app. If this was a browser app,
-    the prompt would be stylized according to the web browser 
-    https://developer.mozilla.org/en-US/docs/Web/API/Window/confirm
-    */
-    const deleting = confirm('Are you sure you want to delete?')
-    if (deleting) {
-      // setShowDeleteAlert(false)
-      setShowSpinner(true)
-      setReady(false)
-      const deletion = await fetch('http://127.0.0.1:5000/flask/delete', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ delete_entry: id })
-      })
-      // https://www.geeksforgeeks.org/javascript/remove-array-element-based-on-object-property-in-javascript/
-      // for fix. match the ID and delete locally alongside database.
-      // avoids a second retrieval and having to possibly retrieve the random seed.
-      let finalEntries = entries
-      let rem = finalEntries.findIndex((finalEntries) => finalEntries.id === id)
-      finalEntries.splice(rem, 1)
-      setEntries(finalEntries)
-      setReady(true)
-      setShowSpinner(false)
-    }
-    // setShowDeleteAlert(false)
-    // setReadyToDelete(false)
+  const executeDelete = async () => {
+    let id = entryToDelete
+    setShowSpinner(true)
+    setReady(false)
+    const deletion = await fetch('http://127.0.0.1:5000/flask/delete', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ delete_entry: id })
+    })
+    // https://www.geeksforgeeks.org/javascript/remove-array-element-based-on-object-property-in-javascript/
+    // for fix. match the ID and delete locally alongside database.
+    // avoids a second retrieval and having to possibly retrieve the random seed.
+    let finalEntries = entries
+    let rem = finalEntries.findIndex((finalEntries) => finalEntries.id === id)
+    finalEntries.splice(rem, 1)
+    setEntries(finalEntries)
+    setReady(true)
+    setShowSpinner(false)
+    handleDeletionClose()
+  }
+
+  const setEntryAndModal = (id) => {
+    setEntryToDelete(id)
+    handleDeletionOpen()
   }
 
   /*
@@ -100,14 +112,13 @@ function App() {
       <h1>
         Add something <span className="react">positive</span> to your day
       </h1>
-      <p>What is something good that happened today?</p>
       <div className="mb-3">
         <form id="queryForm" onSubmit={saveEntry}>
           <input
             className="form-control"
             type="text"
             name="enter-message"
-            placeholder="++++++++++++++++++++++"
+            placeholder="What is something good that happened today?"
             value={entryQuery}
             onChange={handleQueryChange}
           />
@@ -128,7 +139,7 @@ function App() {
                 key={entry.id}
                 id={entry.id}
                 data={entry.data}
-                onDelete={executeDelete}
+                onDelete={setEntryAndModal}
               />
             ))}
           </div>
@@ -140,7 +151,33 @@ function App() {
           <div></div>
         )}
       </div>
-      {/*  <div>{showDeleteAlert ? <DeleteAlert onDelete={setDelete} /> : <div></div>}</div>*/}
+      <Modal show={showDeletionModal} onHide={handleDeletionClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Deletion alert</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete this entry?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={executeDelete}>
+            Yes
+          </Button>
+          <Button variant="secondary" onClick={handleDeletionClose}>
+            No
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showSubmissionModal} onHide={handleSubmissionClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Entry alert</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          You entered: {entryQuery}. Let's take a look at some other positive things you've entered!
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleSubmissionClose}>
+            Ok
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   )
 }
